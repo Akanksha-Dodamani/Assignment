511a5ca881dbf3c6049468a8ece9b4f7931429f2
Akanksha-Dodamani <akanksha.dodamani@aurionpro.com>
2025-07-20 20:14:03 +0530

    Abstarct and interface

---
+++ Abstract&Interface/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Interface
+{
+    internal abstract class Animal : IAnimal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Eat();
+        public abstract void Walk();
+        public override string ToString()
+        {
+            return "Name of animal: ";
+        }
+    }
+}
---
+++ Abstract&Interface/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Abstract_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dogName = new Dog("Austin");
+            dogName.Eat();
+            dogName.Walk();
+            dogName.ToString();
+        }
+    }
+}
