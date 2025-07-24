8156e8ab0aa1be8ba58870d1908775f4fea45c98
Akanksha-Dodamani <akanksha.dodamani@aurionpro.com>
2025-07-24 10:06:57 +0530

    string manipulation code

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Sachin, Aurionpro, Mumbai";
+
+            string[] parts = input.Split(',');
+
+            if (parts.Length == 3)
+            {
+                Console.WriteLine("Name: " + parts[0]);
+                Console.WriteLine("Company: " + parts[1]);
+                Console.WriteLine("Location: " + parts[2]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input string format.");
+            }
+        }
+    }
+}
