a00beed273a3c0179f7767c7de460cbc6fe5fea9
Akanksha-Dodamani <akanksha.dodamani@aurionpro.com>
2025-07-24 10:26:26 +0530

    Class diagram code

---
+++ ClassDiagram/ArrayTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagram
+{
+    internal class ArrayTest
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "Mumbai", "Pune", "Banglore", "Chennai" };
+
+            Console.WriteLine("Using FOR loop:");
+            for (int i = 0; i < cities.Length; i++)
+            {
+                Console.WriteLine("Cities: " + cities[i]);
+            }
+
+            Console.WriteLine("Using FOREACH loop:");
+            foreach (string city in cities)
+            {
+                Console.WriteLine("Cities: " + city);
+            }
+        }
+    }
+}
---
+++ ClassDiagram/LoopTest.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassDiagram
+{
+    internal class LoopTest
+    {
+        static void Main(string[] args)
+        {
+            string name = "Akanksha Dodamani";
+
+            Console.WriteLine("Using FOR loop:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Using WHILE loop:");
+            int j = 0;
+            while (j < 5)
+            {
+                Console.WriteLine(name);
+                j++;
+            }
+        }
+    }
+}
